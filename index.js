e51c1c22d8180c9d7febfbf5389757b3f0e4114a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-07 14:58:07 +0530

    COMPLETED OTHER WAY OF BLOGPOST SCHEMA TO THE BLOG POST PROJECT

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-const express = require('express')
-const path = require('path')
-
-const app = express();
-
-/*WITH THIS CODE,EXPRESS WILL ACCESS ALL 
-THE FOLDERS IN STATIC ASSETS IN THE PUBLIC DIRECTORY */
-app.use(express.static('public'));
-
-app.listen(4000,()=>{
-    console.log('App is Running in 4000 port');
-    
-});
+const express = require('express'); 
 
 
-app.get('/',(req,res) =>{
-    // res.sendFile(path.resolve(__dirname,'pages/index.html'))
-    res.render('index')
+const ejs = require('ejs'); 
 
-});
+const mongoose = require('mongoose');
 
-app.get('/about',(req,res) =>{
-    // res.sendFile(path.resolve(__dirname,'pages/about.html'))
-    res.render('about')
-
-});
-
-app.get('/content',(req,res) =>{
-    // res.sendFile(path.resolve(__dirname,'pages/content.html'))
-    res.render('content')
-
-});
-app.get('/post',(req,res) =>{
-    // res.sendFile(path.resolve(__dirname,'pages/post.html'))
-    res.render('post')
-    
-});
+mongoose.connect('mongodb://localhost/my_database',{useNewUrlPareser : true})
--- test.js
+++ test.js
@@ -6,6 +6,25 @@ const BlogPost = require('./models/BlogPost.js')
 
 mongoose.connect('mongodb://0.0.0.0:27017/my_database');
 
+//Returning all the documents inside BlogPost Collection
+BlogPost.find({},(error,blogpost)=>{
+    console.log(error,blogpost);
+    
+})
+
+//Using Regex to fine tune the search
+BlogPost.find({title: /online/},(error,blogpost)=>{
+    console.log(error,blogpost);
+    
+})
+
+//Using unique ID to search the document
+BlogPost.findById(id,(error,blogpost)=>{
+    console.log(error,blogpost);
+    
+})
+
+/*
 const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
@@ -22,3 +41,4 @@ BlogPost.create({
     console.log(error,blogpost);
     
 });
+*/
